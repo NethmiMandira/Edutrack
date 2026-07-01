37f4557d26408d773eec6d70e35644b6f6966599
NethmiMandira <nethmimandira273@gmail.com>
2026-07-01 20:48:09 +0530

    fix: always show horizontal scrollbar in StudentGrid desktop view

--- client/src/pages/StudentRegistration.jsx
+++ client/src/pages/StudentRegistration.jsx
@@ -363,7 +363,8 @@ export default function StudentRegistration() {
             />
           </div>
 
-          <div className="mt-12 w-full" ref={gridRef}>
+          {/* MODIFIED: added overflow-x-scroll to force horizontal scrollbar */}
+          <div className="mt-12 w-full overflow-x-scroll" ref={gridRef}>
             {loading ? (
               <div className="flex flex-col items-center justify-center py-20 gap-4">
                 <div className="w-12 h-12 border-4 border-indigo-500 border-t-transparent rounded-full animate-spin" />
